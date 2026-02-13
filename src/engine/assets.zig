6fd9237383bbea6d84157bb879a8e7ff0586c259
Ali Esmaili Seraji <faramin.smile@gmail.com>
2026-02-13 21:40:00 +0330

    Fix memory error: now AssetManager, manage its own keys memory.

--- src/engine/assets.zig
+++ src/engine/assets.zig
@@ -17,62 +17,92 @@ pub const AssetManager = struct {
 
     pub fn deinit(self: *Self) void {
         {
-            var it = self.textures.valueIterator();
-            while (it.next()) |v| _ = rl.UnloadTexture(v.*);
+            var it = self.textures.iterator();
+            while (it.next()) |entry| {
+                rl.UnloadTexture(entry.value_ptr.*);
+                self.gpa.free(entry.key_ptr.*);
+            }
             self.textures.deinit();
         }
         {
-            var it = self.models.valueIterator();
-            while (it.next()) |v| _ = v.unload();
+            var it = self.models.iterator();
+            while (it.next()) |entry| {
+                entry.value_ptr.unload();
+                self.gpa.free(entry.key_ptr.*);
+            }
             self.models.deinit();
         }
         {
-            var it = self.shaders.valueIterator();
-            while (it.next()) |v| _ = rl.UnloadShader(v.*);
+            var it = self.shaders.iterator();
+            while (it.next()) |entry| {
+                rl.UnloadShader(entry.value_ptr.*);
+                self.gpa.free(entry.key_ptr.*);
+            }
             self.shaders.deinit();
         }
     }
 
     pub fn loadTexture(self: *Self, key: []const u8, filename: [:0]const u8) !*rl.Texture2D {
-        if (self.textures.get(key)) |t| rl.UnloadTexture(t);
         const texture = rl.LoadTexture(@ptrCast(filename));
-        try self.textures.put(key, texture);
-        return self.textures.getPtr(key).?;
+        if (self.textures.getPtr(key)) |ptr| {
+            rl.UnloadTexture(ptr.*);
+            ptr.* = texture;
+            return ptr;
+        }
+        const key_copy = try self.gpa.dupe(u8, key);
+        errdefer self.gpa.free(key_copy);
+        try self.textures.put(key_copy, texture);
+        return self.textures.getPtr(key_copy).?;
     }
 
     pub fn unloadTexture(self: *Self, key: []const u8) bool {
-        const ptr = self.textures.getPtr(key) orelse return false;
-        rl.UnloadTexture(ptr.*);
-        return self.textures.remove(key);
+        const entry = self.textures.fetchRemove(key) orelse return false;
+        rl.UnloadTexture(entry.value);
+        self.gpa.free(entry.key);
+        return true;
     }
 
     pub fn loadModel(self: *Self, key: []const u8, filename: [:0]const u8) !*Model {
-        if (self.models.getPtr(key)) |m| m.unload();
         const model = Model.load(filename);
-        try self.models.put(key, model);
-        return self.models.getPtr(key).?;
+        if (self.models.getPtr(key)) |m| {
+            m.unload();
+            m.* = model;
+            return m;
+        }
+        const key_copy = try self.gpa.dupe(u8, key);
+        errdefer self.gpa.free(key_copy);
+        try self.models.put(key_copy, model);
+        return self.models.getPtr(key_copy).?;
     }
 
     pub fn unloadModel(self: *Self, key: []const u8) bool {
-        const ptr = self.models.getPtr(key) orelse return false;
-        ptr.unload();
-        return self.models.remove(key);
+        const entry = self.models.fetchRemove(key) orelse return false;
+        entry.value.unload();
+        self.gpa.free(entry.key);
+        return true;
     }
 
     pub fn loadShader(self: *Self, key: []const u8, vs_filename: [:0]const u8, fs_filename: [:0]const u8) !*rl.Shader {
-        if (self.shaders.get(key)) |s| rl.UnloadShader(s);
         const shader = rl.LoadShader(
             @ptrCast(vs_filename),
             @ptrCast(fs_filename),
         );
-        try self.shaders.put(key, shader);
-        return self.shaders.getPtr(key).?;
+        if (self.shaders.getPtr(key)) |ptr| {
+            rl.UnloadShader(ptr.*);
+            ptr.* = shader;
+            return ptr;
+        }
+        const key_copy = try self.gpa.dupe(u8, key);
+        errdefer self.gpa.free(key_copy);
+        try self.shaders.put(key_copy, shader);
+        return self.shaders.getPtr(key_copy).?;
     }
 
     pub fn unloadShader(self: *Self, key: []const u8) bool {
-        const ptr = self.shaders.getPtr(key) orelse return false;
-        rl.UnloadShader(ptr.*);
-        return self.textures.remove(key);
+        const entry = self.shaders.fetchRemove(key) orelse return false;
+        rl.UnloadShader(entry.value);
+        self.gpa.free(entry.key);
+        return true;
     }
 };
 
